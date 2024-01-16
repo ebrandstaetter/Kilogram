514401cf479e59eb753599bcccea2fa30f8fb0ff
KonstantinOeller <k.oeller@students.htl-leonding.ac.at>
2024-01-16 17:38:08 +0100

    changed script structure

--- public/script.js
+++ public/script.js
@@ -1,53 +1,54 @@
 generateAllRecepies();
 function generateAllRecepies() {
-    let recepiesHTML = "";
 
     fetch('http://localhost:3000/getAllPosts')
     .then(response => response.json())
     .then(recepies => {
         console.log(recepies);
-
-
-        for (let i = 0; i < recepies.posts.length; i++) {
-            let recepie = recepies.posts[i];
-            let tagsHTML = "";
-            for (let j = 0; j < recepie.tags.length; j++) {
-                tagsHTML += `#${recepie.tags[j]} `;
-            }
-            let ingredientsHTML = "";
-            for (let j = 0; j < recepie.ingredients.length; j++) {
-                ingredientsHTML += `${recepie.ingredients[j]}`;
-                if (j < recepie.ingredients.length - 1) {
-                    ingredientsHTML += ` | `;
-                }
-            }
-            let recepieHTML = `
-                <div class="postImage"></div>
-                <div class="postContent">
-                    <h1 class="postTitle">${recepie.title}</h1>
-                    <div class="postRating"></div>
-                    <p class="postDescription">${recepie.description}</p>
-                    <h2>Ingredients:</h2>
-                    <p class="postIngredients">${ingredientsHTML}</p>
-                    <p class="postPreparation">${recepie.preparation}</p>
-                    <div class="postFooter">
-                        <p class="postTags">${tagsHTML}</p>
-                        <p class="postDate">Posted on: ${recepie.date}</p>
-                        <img class="postUserIcon" src="../Kilogram/Assets/Images/default_profile_icon.png">
-                        <p class="postUserName">${recepie.userName}</p>
-                    </div>
-                </div>
-            `;
-            console.log(recepieHTML);
-            recepiesHTML += recepieHTML;
-        }
-        
-        console.log(recepiesHTML);
-        document.querySelector(".postCard").innerHTML = recepiesHTML;
+        document.querySelector(".postCard").innerHTML = generatePostsHtml(recepies);
     })
     .catch(err => console.log(err));
 }
 
+function generatePostsHtml(recepies) {
+    let recepiesHTML = "";
+
+    for (let i = 0; i < recepies.posts.length; i++) {
+        let recepie = recepies.posts[i];
+        let tagsHTML = "";
+        for (let j = 0; j < recepie.tags.length; j++) {
+            tagsHTML += `#${recepie.tags[j]} `;
+        }
+        let ingredientsHTML = "";
+        for (let j = 0; j < recepie.ingredients.length; j++) {
+            ingredientsHTML += `${recepie.ingredients[j]}`;
+            if (j < recepie.ingredients.length - 1) {
+                ingredientsHTML += ` | `;
+            }
+        }
+        let recepieHTML = `
+            <div class="postImage"></div>
+            <div class="postContent">
+                <h1 class="postTitle">${recepie.title}</h1>
+                <div class="postRating"></div>
+                <p class="postDescription">${recepie.description}</p>
+                <h2>Ingredients:</h2>
+                <p class="postIngredients">${ingredientsHTML}</p>
+                <p class="postPreparation">${recepie.preparation}</p>
+                <div class="postFooter">
+                    <p class="postTags">${tagsHTML}</p>
+                    <p class="postDate">Posted on: ${recepie.date}</p>
+                    <img class="postUserIcon" src="../Kilogram/Assets/Images/default_profile_icon.png">
+                    <p class="postUserName">${recepie.userName}</p>
+                </div>
+            </div>
+        `;
+        console.log(recepieHTML);
+        recepiesHTML += recepieHTML;
+    }
+
+    return recepiesHTML;
+}
 
 
 
