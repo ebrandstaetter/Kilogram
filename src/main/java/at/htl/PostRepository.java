de8104071d9699db5d0d1692f554dd300e48a4ee
“[KonstantinOeller]” <[k.oeller@students.htl-leonding.ac.at]>
2024-05-28 15:14:07 +0200

    Post Repository fixed

--- src/main/java/at/htl/PostRepository.java
+++ src/main/java/at/htl/PostRepository.java
@@ -2,6 +2,8 @@ package at.htl;
 
 import at.htl.model.Post;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
 
+@ApplicationScoped
 public class PostRepository implements PanacheRepository<Post> {
 }
